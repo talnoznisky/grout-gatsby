44bb9e2c3f0294860f1a370c7e9710af59741c64
talnoznisky <tal.noznisky@gmail.com>
2021-09-09 19:46:41 -0500

    writes in selected sequence

--- main.py
+++ main.py
@@ -14,19 +14,19 @@ soup = BeautifulSoup(data, 'html.parser')
 
 reg = re.compile('chapter-*')
 text = soup.find_all('p')
-sentences = list(itertools.chain(*[nltk.sent_tokenize(l.text) for l in text]))
+paragraphs = [nltk.sent_tokenize(l.text) for l in text]
+sentences = list(itertools.chain(*paragraphs))
 
 alpha_tokens = sorted(sentences, key=lambda s: s if s[0].isalnum() else s[1:])
 len_tokens = sorted(sentences, key=lambda s: len(s))
 
 lines = []
 i = 0
-for t in sentences:
+for i,t in enumerate(sentences):
     obj =   {
-                "line": t,
-                "default_position": i,
-                "len_position": len_tokens.index(t),
-                "alpha_tokens": alpha_tokens.index(t)
+                "default": t,
+                "length": len_tokens[i],
+                "alpha": alpha_tokens[i]
             }
     lines.append(obj)
     i+=1
@@ -35,3 +35,15 @@ with open('lines.json','w+', encoding='utf-8') as f:
     json.dump(lines, f, ensure_ascii=False)
 
 
+i = 0
+p_indices = []
+for p in paragraphs:
+    l = []
+    for k in range(len(p)):
+        l.append(i)
+        i+=1
+    p_indices.append(l)
+
+
+with open('p_indices.json', 'w+') as f:
+    json.dump(p_indices, f)
