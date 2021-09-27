b7db2d23deba9a17c6f6311077b5a65a9c3c96ab
talnoznisky <tal.noznisky@gmail.com>
2021-09-26 22:17:24 -0500

    update with spacy

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import itertools
 import json
 import re
-from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+import spacy
+
+from spacy.language import Language
 from bs4 import BeautifulSoup
 
-punkt_param = PunktParameters()
-punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
-sentence_splitter = PunktSentenceTokenizer(punkt_param)
+@Language.component('prevent-sbd')
+def prevent_sbd(doc):
+    """ Ensure that SBD does not run on tokens inside quotation marks and brackets. """
+    quote_open = False
+    bracket_open = False
+    can_sbd = True
+    for token in doc:
+        # Don't do sbd on these tokens
+        if not can_sbd:
+            token.is_sent_start = False
+
+        # Not using .is_quote so that we don't mix-and-match different kinds of quotes (e.g. ' and ")
+        # Especially useful since quotes don't seem to work 
+        # well with .is_left_punct or .is_right_punct
+        if token.text == '"':
+            quote_open = False if quote_open else True
+        elif token.is_bracket and token.is_left_punct:
+            bracket_open = True
+        elif token.is_bracket and token.is_right_punct:
+            bracket_open = False
+
+        can_sbd = not (quote_open or bracket_open)
+
+    return doc
+
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'textcat'])
+nlp.add_pipe('prevent-sbd', before='parser')
 
 with open('greatgatsby.html', 'r') as f:
     data = f.read()
@@ -15,10 +41,19 @@ soup = BeautifulSoup(data, 'html.parser')
 
 reg = re.compile('chapter-*')
 text = soup.find_all('p')
-paragraphs = [sentence_splitter.tokenize(l.text.replace('?"', '? "').replace('!"', '! "').replace('."', '. "')) for l in text]
-sentences = list(itertools.chain(*paragraphs))
+paragraphs = [t.text for t in text]
+docs = list(nlp.pipe(paragraphs))
 
-sentences = [text.replace('? "', '?"').replace('! "', '!"').replace('. "', '."') for text in sentences]
+i=0
+sentences = []
+p_indices = []
+for doc in docs:
+    l = []
+    for sent in doc.sents:
+        sentences.append(sent.text)
+        l.append(i)
+        i+=1
+    p_indices.append(l)
 
 alpha_tokens = sorted(sentences, key=lambda s: s if s[0].isalnum() else s[1:])
 len_tokens = sorted(sentences, key=lambda s: len(s))
@@ -36,17 +71,6 @@ for i,t in enumerate(sentences):
 
 with open('lines.json','w+', encoding='utf-8') as f:
     json.dump(lines, f, ensure_ascii=False)
-
-
-i = 0
-p_indices = []
-for p in paragraphs:
-    l = []
-    for k in range(len(p)):
-        l.append(i)
-        i+=1
-    p_indices.append(l)
-
 
 with open('p_indices.json', 'w+') as f:
     json.dump(p_indices, f)
